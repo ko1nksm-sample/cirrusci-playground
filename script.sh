4835e5a40c2aa3f6252b6e880d9ef800b8b74634
Koichi Nakashima <koichi@nksm.name>
2019-07-18 10:45:51 +0900

    update

--- script.sh
+++ script.sh
@@ -7,21 +7,31 @@ mount
 
 touch files/readable
 chmod a=,u+r files/readable
+[ -r files/readable ]
+echo "files/readable is readable $?"
 
 touch files/writable
 chmod a=,u+w files/writable
+[ -w files/writable ]
+echo "files/writable is writable $?"
 
 touch files/executable
 chmod a=,u+x files/executable
+[ -x files/executable ]
+echo "files/executable is executable $?"
 
 touch files/no-permission
 chmod a= files/no-permission
 
 touch files/setuid
 chmod a=,u+s files/setuid
+[ -u files/setuid ]
+echo "files/setuid is setuid $?"
 
 touch files/setgid
 chmod a=,g+s files/setgid
+[ -g files/setgid ]
+echo "files/setgid is setgid $?"
 
 echo
 
